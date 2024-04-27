179bda820a06e32f55af30756f5c75c8e21d9329
Gleb Zagovalski <gurzza@list.ru>
2024-04-27 15:01:02 +0300

    simple sign bulletin with candidates

--- main/signing_bulletin.py
+++ main/signing_bulletin.py
@@ -1,7 +1,15 @@
 import ast
+import json
+import os
+
+from Crypto.PublicKey import RSA
+from common_functions import *
 
 with open('../ca_cert/bulletin.key', 'r') as f:
     bulletin_pr_key = f.read()
+
+with open('../ca_cert/bulletin.cer', 'r') as f:
+    bulletin_pub_key = f.read()
 
 with open('../bulletin/candidates', 'r') as f:
     candidates_list = f.read()
@@ -13,5 +21,16 @@ for key in candidates_dict:
 
 
 is_agree = bool(input('Are you sure that you want to sign this list? Enter any number if you are agree, else skip: '))
-#if is_agree:
-# add sign with password
+if is_agree:
+    candidates_json_s = json.dumps(candidates_dict)
+    #print(type(candidates_json_s))
+    #print(candidates_list)
+    s = sign_data(candidates_json_s, bulletin_pr_key, with_password=True)
+    with open('../bulletin/candidates_signed', 'wb') as f:
+        f.write(s)
+    print(verify_sign(candidates_json_s, s, bulletin_pub_key))
+
+else:
+    print('Something wrong with list of candidates!')
+    os.remove('../bulletin/candidates_signed')
+
